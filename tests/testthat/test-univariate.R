5f61c451bcca0d8f58d0adeeb084d9287ce6a687
Robin Genuer <Robin.Genuer@u-bordeaux.fr>
2018-03-05 22:26:16 +0100

    remove raw et rox

--- tests/testthat/test-univariate.R
+++ tests/testthat/test-univariate.R
@@ -1,8 +1,8 @@
 context("Univariate gaussian test")
 
 test_that("correct result for univariate gaussian", {
-  expect_equal(mvnpdf(x=matrix(1.96), mean=0, varcovM=diag(1), Log=FALSE)$y,
+  expect_equal(mvnpdf(x=matrix(1.96), Log=FALSE)$y,
                dnorm(1.96))
-  expect_equal(mvnpdf(x=matrix(c(1.96, -0.5), ncol = 2), mean=0, varcovM=diag(1), Log=FALSE)$y,
+  expect_equal(mvnpdf(x=matrix(c(1.96, -0.5), ncol = 2), Log=FALSE)$y,
                dnorm(c(1.96, -0.5)))
 })
